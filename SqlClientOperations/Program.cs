64f9db46bf0ef44d64ab108e548b86548ca28bf1
Huseyin6 <hsynsimsek723@gmail.com>
2020-09-11 09:39:58 +0300

    init

---
+++ SqlClientOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlClientOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
